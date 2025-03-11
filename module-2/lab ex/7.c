f5cbc92294d50a9a23d07d9a78eed322d2f33081
sutharprince30 <princesutharlp999@gmail.com>
2025-03-11 22:31:55 +0530

    7.c

--- module-2/lab ex/7.c
+++ module-2/lab ex/7.c
@@ -1,6 +1,6 @@
 /*Write a C program that uses the break statement to stop printing numbers
 when it reaches 5. Modify the program to skip printing the number 3 using the
-continue statement.
+continue statement.*/
 
 
 #include <stdio.h>
